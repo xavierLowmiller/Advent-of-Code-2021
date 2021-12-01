dc5d5cb6d642f7d955cb3c32dafb2a760c023d1f
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-01 07:46:26 +0100

    Solve Day 1

--- Day1/Sources/Day1/Day1.swift
+++ Day1/Sources/Day1/Day1.swift
@@ -1,6 +1,14 @@
-public struct Day1 {
-    public private(set) var text = "Hello, World!"
+func numberOfIncreases(_ input: [Int]) -> Int {
+    zip(input, input.dropFirst()).filter { $0 < $1 }.count
+}
 
-    public init() {
+func numberOfTripleIncreases(_ input: [Int]) -> Int {
+    numberOfIncreases(input.triples)
+}
+
+private extension Sequence where Element == Int {
+    var triples: [Int] {
+        zip(zip(self, self.dropFirst(1)), self.dropFirst(2))
+            .map { $0.0 + $0.1 + $1 }
     }
 }
--- Day1/Tests/Day1Tests/Day1Tests.swift
+++ Day1/Tests/Day1Tests/Day1Tests.swift
@@ -2,10 +2,45 @@ import XCTest
 @testable import Day1
 
 final class Day1Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day1().text, "Hello, World!")
+    func testDay1Part1Example() throws {
+        let input = [
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263
+        ]
+
+        XCTAssertEqual(numberOfIncreases(input), 7)
+    }
+
+    func testDay1Part1() {
+        print("Day 1 Part 1:", numberOfIncreases(input))
+    }
+
+    func testDay1Part2Example() throws {
+        let input = [
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263
+        ]
+
+        XCTAssertEqual(numberOfTripleIncreases(input), 5)
+    }
+
+    func testDay1Part2() {
+        print("Day 1 Part 2:", numberOfTripleIncreases(input))
     }
 }
