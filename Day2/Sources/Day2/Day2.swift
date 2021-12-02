905b7fbec9d2537781515ce44ef197f9c8b7d95b
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-02 06:57:36 +0100

    Solve Day 2

--- Day2/Sources/Day2/Day2.swift
+++ Day2/Sources/Day2/Day2.swift
@@ -1,6 +1,40 @@
-public struct Day2 {
-    public private(set) var text = "Hello, World!"
+struct Submarine {
+    var horizontalPosition = 0
+    var depth = 0
+    var aim = 0
 
-    public init() {
+    mutating func navigate<S: StringProtocol>(_ command: S) {
+        let parts = command.split(separator: " ")
+        let direction = parts[0]
+        let amount = Int(parts[1])
+
+        switch (direction, amount) {
+        case ("forward", let amount?):
+            horizontalPosition += amount
+        case ("down", let amount?):
+            depth += amount
+        case ("up", let amount?):
+            depth -= amount
+        default:
+            fatalError()
+        }
+    }
+
+    mutating func navigateWithAim<S: StringProtocol>(_ command: S) {
+        let parts = command.split(separator: " ")
+        let direction = parts[0]
+        let amount = Int(parts[1])
+
+        switch (direction, amount) {
+        case ("forward", let amount?):
+            horizontalPosition += amount
+            depth += amount * aim
+        case ("down", let amount?):
+            aim += amount
+        case ("up", let amount?):
+            aim -= amount
+        default:
+            fatalError()
+        }
     }
 }
--- Day2/Tests/Day2Tests/Day2Tests.swift
+++ Day2/Tests/Day2Tests/Day2Tests.swift
@@ -2,10 +2,63 @@ import XCTest
 @testable import Day2
 
 final class Day2Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day2().text, "Hello, World!")
+    func testDay2Part1Example() throws {
+        let input = """
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+        """.split(separator: "\n")
+        var submarine = Submarine()
+
+        for command in input {
+            submarine.navigate(command)
+        }
+
+        XCTAssertEqual(submarine.horizontalPosition, 15)
+        XCTAssertEqual(submarine.depth, 10)
+        XCTAssertEqual(submarine.horizontalPosition * submarine.depth, 150)
+    }
+
+    func testDay2Part1() throws {
+        var submarine = Submarine()
+
+        for command in input.split(separator: "\n") {
+            submarine.navigate(command)
+        }
+
+        print("Day 2 Part 1:", submarine.horizontalPosition * submarine.depth)
+    }
+
+    func testDay2Part2Example() throws {
+        let input = """
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+        """.split(separator: "\n")
+        var submarine = Submarine()
+
+        for command in input {
+            submarine.navigateWithAim(command)
+        }
+
+        XCTAssertEqual(submarine.horizontalPosition, 15)
+        XCTAssertEqual(submarine.depth, 60)
+        XCTAssertEqual(submarine.horizontalPosition * submarine.depth, 900)
+    }
+
+    func testDay2Part2() throws {
+        var submarine = Submarine()
+
+        for command in input.split(separator: "\n") {
+            submarine.navigateWithAim(command)
+        }
+
+        print("Day 2 Part 2:", submarine.horizontalPosition * submarine.depth)
     }
 }
