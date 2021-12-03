8411f3fd4f88e31386fbf334905c9caab17ad0ee
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-03 07:22:47 +0100

    Solve Day 3

--- Day3/Sources/Day3/Day3.swift
+++ Day3/Sources/Day3/Day3.swift
@@ -1,6 +1,66 @@
-public struct Day3 {
-    public private(set) var text = "Hello, World!"
+func powerConsumption(of report: [String]) -> Int {
+    let report = report.map(Array.init)
+    return report.gamma * report.epsilon
+}
 
-    public init() {
+func lifeSupportRating(of report: [String]) -> Int {
+    let report = report.map(Array.init)
+    return report.oxygenGeneratorRating() * report.co2ScrubberRating()
+}
+
+private extension Array where Element == [Character] {
+    var gamma: Int {
+        let bits = self[0].indices.map(mostCommonBit)
+
+        return Int(String(bits), radix: 2)!
+    }
+
+    var epsilon: Int {
+        let bits = self[0].indices.map(leastCommonBit)
+        return Int(String(bits), radix: 2)!
+    }
+
+    func oxygenGeneratorRating(index: Int = 0) -> Int {
+        guard count > 1 else { return Int(String(self[0]), radix: 2)! }
+
+        let mostCommonBit = mostCommonBit(at: index)
+
+        return filter { $0[index] == mostCommonBit }
+            .oxygenGeneratorRating(index: index + 1)
+    }
+
+    func co2ScrubberRating(index: Int = 0) -> Int {
+        guard count > 1 else { return Int(String(self[0]), radix: 2)! }
+
+        let leastCommonBit = leastCommonBit(at: index)
+
+        return filter { $0[index] == leastCommonBit }
+            .co2ScrubberRating(index: index + 1)
+    }
+
+    func mostCommonBit(at index: Int) -> Character {
+        var ones: Int = 0
+        var zeroes: Int = 0
+        for element in self {
+            if element[index] == "1" {
+                ones += 1
+            } else {
+                zeroes += 1
+            }
+        }
+        return ones >= zeroes ? "1" : "0"
+    }
+
+    func leastCommonBit(at index: Int) -> Character {
+        var ones: Int = 0
+        var zeroes: Int = 0
+        for element in self {
+            if element[index] == "1" {
+                ones += 1
+            } else {
+                zeroes += 1
+            }
+        }
+        return ones < zeroes ? "1" : "0"
     }
 }
--- Day3/Tests/Day3Tests/Day3Tests.swift
+++ Day3/Tests/Day3Tests/Day3Tests.swift
@@ -2,10 +2,49 @@ import XCTest
 @testable import Day3
 
 final class Day3Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day3().text, "Hello, World!")
+    func testDay3Part1Example() throws {
+        let input = """
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+        """.components(separatedBy: "\n")
+
+        XCTAssertEqual(powerConsumption(of: input), 198)
+    }
+
+    func testDay3Part1() throws {
+        print("Day 3 Part 1:", powerConsumption(of: input))
+    }
+
+    func testDay3Part2Example() throws {
+        let input = """
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+        """.components(separatedBy: "\n")
+
+        XCTAssertEqual(lifeSupportRating(of: input), 230)
+    }
+
+    func testDay3Part2() throws {
+        print("Day 3 Part 2:", lifeSupportRating(of: input))
     }
 }
