2f79afd1f55de55c8d41999a01db63cb2a500943
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-06 06:25:06 +0100

    Solve Day 6

--- Day6/Sources/Day6/Day6.swift
+++ Day6/Sources/Day6/Day6.swift
@@ -1,6 +1,25 @@
-public struct Day6 {
-    public private(set) var text = "Hello, World!"
+struct SchoolOfFish {
+    private(set) var state: [Int]
+    private let maxValue: Int
 
-    public init() {
+    init(fish: [Int], maxValue: Int = 8) {
+        state = Array(repeating: 0, count: maxValue + 1)
+        self.maxValue = maxValue
+        for fish in fish {
+            state[fish] += 1
+        }
+    }
+
+    mutating func reproduce() {
+        let amountOfNewFish = state[0]
+        for key in 0..<maxValue {
+            state[key] = state[key + 1]
+        }
+        state[6] += amountOfNewFish
+        state[8] = amountOfNewFish
+    }
+
+    var totalFish: Int {
+        state.reduce(0, +)
     }
 }
--- Day6/Tests/Day6Tests/Day6Tests.swift
+++ Day6/Tests/Day6Tests/Day6Tests.swift
@@ -2,10 +2,57 @@ import XCTest
 @testable import Day6
 
 final class Day6Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day6().text, "Hello, World!")
+    func testSinlgeReproduction1() {
+        var school = SchoolOfFish(fish: [3, 4, 3, 1, 2])
+        school.reproduce()
+        XCTAssertEqual(school.state, [1, 1, 2, 1, 0, 0, 0, 0, 0])
+    }
+
+    func testSinlgeReproduction2() {
+        var school = SchoolOfFish(fish: [2, 3, 2, 0, 1])
+        school.reproduce()
+        XCTAssertEqual(school.state, [1, 2, 1, 0, 0, 0, 1, 0, 1])
+    }
+
+    func testDay6Part1Example() throws {
+        let input = "3,4,3,1,2"
+        var school = SchoolOfFish(fish: input.split(separator: ",").compactMap { Int($0) })
+
+        for _ in 0..<80 {
+            school.reproduce()
+        }
+
+        XCTAssertEqual(school.totalFish, 5934)
+    }
+
+    func testDay6Part1() throws {
+        var school = SchoolOfFish(fish: input.split(separator: ",").compactMap { Int($0) })
+
+        for _ in 0..<80 {
+            school.reproduce()
+        }
+
+        print("Day 6, Part 1:", school.totalFish)
+    }
+
+    func testDay6Part2Example() throws {
+        let input = "3,4,3,1,2"
+        var school = SchoolOfFish(fish: input.split(separator: ",").compactMap { Int($0) })
+
+        for _ in 0..<256 {
+            school.reproduce()
+        }
+
+        XCTAssertEqual(school.totalFish, 26984457539)
+    }
+
+    func testDay6Part2() throws {
+        var school = SchoolOfFish(fish: input.split(separator: ",").compactMap { Int($0) })
+
+        for _ in 0..<256 {
+            school.reproduce()
+        }
+
+        print("Day 6, Part 2:", school.totalFish)
     }
 }
