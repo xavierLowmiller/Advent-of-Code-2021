baeacef548ef1bf47425e14d5dfaea9e8f35f10e
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-07 07:52:50 +0100

    Solve Day 7

--- Day7/Sources/Day7/Day7.swift
+++ Day7/Sources/Day7/Day7.swift
@@ -1,6 +1,26 @@
-public struct Day7 {
-    public private(set) var text = "Hello, World!"
+extension Array where Element == Int {
+    private var median: Int {
+        sorted()[count / 2]
+    }
 
-    public init() {
+    var fuelConsumptionToMedian: Int {
+        let median = self.median
+        return reduce(0) { $0 + abs(median - $1) }
+    }
+
+    var fuelConsumptionToCommonClosestPoint: Int {
+        guard let min = self.min(),
+              let max = self.max()
+        else { return 0 }
+
+        return (min...max).map { p in
+            reduce(0) { $0 + abs(p - $1).triangular }
+        }.min()!
+    }
+}
+
+private extension Int {
+    var triangular: Int {
+        return (self * (self + 1)) / 2
     }
 }
--- Day7/Tests/Day7Tests/Day7Tests.swift
+++ Day7/Tests/Day7Tests/Day7Tests.swift
@@ -2,10 +2,23 @@ import XCTest
 @testable import Day7
 
 final class Day7Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day7().text, "Hello, World!")
+    func testDay7Part1Example() throws {
+        let input = "16,1,2,0,4,2,7,1,2,14".split(separator: ",").compactMap( { Int($0) })
+
+        XCTAssertEqual(input.fuelConsumptionToMedian, 37)
+    }
+
+    func testDay7Part1() throws {
+        print("Day 7 Part 1:", input.fuelConsumptionToMedian)
+    }
+
+    func testDay7Part2Example() throws {
+        let input = "16,1,2,0,4,2,7,1,2,14".split(separator: ",").compactMap( { Int($0) })
+
+        XCTAssertEqual(input.fuelConsumptionToCommonClosestPoint, 168)
+    }
+
+    func testDay7Part2() throws {
+        print("Day 7 Part 2:", input.fuelConsumptionToCommonClosestPoint)
     }
 }
