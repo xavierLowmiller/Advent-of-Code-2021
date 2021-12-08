a1374bd62189f0e2901519fbab18736585a2dbb3
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-08 07:25:50 +0100

    Solve Day 8

--- Day8/Sources/Day8/Day8.swift
+++ Day8/Sources/Day8/Day8.swift
@@ -1,6 +1,103 @@
-public struct Day8 {
-    public private(set) var text = "Hello, World!"
+struct Note {
+    let output: [Int]
 
-    public init() {
+    var outputValue: Int {
+        output.reduce(0) { $0 * 10 + $1 }
+    }
+
+    init<S: StringProtocol>(string: S) {
+        let inputStrings = string
+            .split(separator: "|")[0]
+            .split(whereSeparator: \.isWhitespace)
+        let outputStrings = string
+            .split(separator: "|")[1]
+            .split(whereSeparator: \.isWhitespace)
+
+        let display = SevenSegmentDisplay(examples: inputStrings + outputStrings)
+
+        output = outputStrings.compactMap(display.value)
+    }
+}
+
+struct SevenSegmentDisplay<S: StringProtocol> {
+    private var lookUpTable: [Set<Character>: Int] = [:]
+    init(examples: [S]) {
+        var examples = Set(examples.map(Set.init))
+        assert(examples.count == 10)
+
+        var one: Set<Character>!
+        var three: Set<Character>!
+        var five: Set<Character>!
+        var eight: Set<Character>!
+        var nine: Set<Character>!
+
+        // Find trivial solutions
+        for example in examples {
+            switch example.count {
+            case 2:
+                lookUpTable[example] = 1
+                one = example
+            case 3:
+                lookUpTable[example] = 7
+            case 4:
+                lookUpTable[example] = 4
+            case 7:
+                lookUpTable[example] = 8
+                eight = example
+            default: continue
+            }
+        }
+        examples.subtract(lookUpTable.keys)
+        assert(examples.count == 6)
+
+        // Find threes
+        for example in examples where example.count == 5 {
+            if example.isSuperset(of: one) {
+                lookUpTable[example] = 3
+                three = example
+            }
+        }
+        examples.subtract(lookUpTable.keys)
+        assert(examples.count == 5)
+
+        // Find nines
+        for example in examples where example.count == 6 {
+            if example.isSuperset(of: three) {
+                lookUpTable[example] = 9
+                nine = example
+            }
+        }
+        examples.subtract(lookUpTable.keys)
+        assert(examples.count == 4)
+
+        // Find twos and fives
+        let lowerLeftBit = eight.subtracting(nine)
+        for example in examples where example.count == 5 {
+            if example.isSuperset(of: lowerLeftBit) {
+                lookUpTable[example] = 2
+            } else {
+                lookUpTable[example] = 5
+                five = example
+            }
+        }
+        examples.subtract(lookUpTable.keys)
+        assert(examples.count == 2)
+
+        // Find zeroes and sixes
+        let topRightBit = eight
+            .subtracting(five)
+            .subtracting(lowerLeftBit)
+
+        for example in examples {
+            if example.isSuperset(of: topRightBit) {
+                lookUpTable[example] = 0
+            } else {
+                lookUpTable[example] = 6
+            }
+        }
+    }
+
+    func value(from string: S) -> Int? {
+        lookUpTable[Set(string)]
     }
 }
--- Day8/Tests/Day8Tests/Day8Tests.swift
+++ Day8/Tests/Day8Tests/Day8Tests.swift
@@ -2,10 +2,78 @@ import XCTest
 @testable import Day8
 
 final class Day8Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day8().text, "Hello, World!")
+    func testDay8Part1Example() throws {
+        let input = """
+        be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+        """.split(separator: "\n")
+
+        let notes = input.map(Note.init)
+        let trivialNumbers: Set<Int> = [1, 4, 7, 8]
+        XCTAssertEqual(notes.flatMap(\.output).filter(trivialNumbers.contains).count, 26)
+    }
+
+    func testDay8Part1() throws {
+        let notes = input.map(Note.init)
+        let trivialNumbers: Set<Int> = [1, 4, 7, 8]
+        print("Day 8 Part 1:", notes.flatMap(\.output).filter(trivialNumbers.contains).count)
+    }
+
+    func testSevenSegmentDisplay() {
+        let input = """
+        acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab cdfeb fcadb cdfeb cdbaf
+        """.split(separator: " ")
+
+        let display = SevenSegmentDisplay(examples: input)
+        XCTAssertEqual(display.value(from: "cagedb"), 0)
+        XCTAssertEqual(display.value(from: "ab"), 1)
+        XCTAssertEqual(display.value(from: "gcdfa"), 2)
+        XCTAssertEqual(display.value(from: "fbcad"), 3)
+        XCTAssertEqual(display.value(from: "eafb"), 4)
+        XCTAssertEqual(display.value(from: "cdfbe"), 5)
+        XCTAssertEqual(display.value(from: "cdfgeb"), 6)
+        XCTAssertEqual(display.value(from: "dab"), 7)
+        XCTAssertEqual(display.value(from: "acedgfb"), 8)
+        XCTAssertEqual(display.value(from: "cefabd"), 9)
+    }
+
+    func testDay8Part2ExampleA() throws {
+        let input = """
+        acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
+        """
+
+        let note = Note(string: input)
+        XCTAssertEqual(note.outputValue, 5353)
+    }
+
+    func testDay8Part2ExampleB() throws {
+        let input = """
+        be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+        """.split(separator: "\n")
+
+        let notes = input.map(Note.init)
+        XCTAssertEqual(notes.map(\.outputValue).reduce(0, +), 61229)
+    }
+
+    func testDay8Part2() throws {
+        let notes = input.map(Note.init)
+        print("Day 8 Part 2:", notes.map(\.outputValue).reduce(0, +))
     }
 }
