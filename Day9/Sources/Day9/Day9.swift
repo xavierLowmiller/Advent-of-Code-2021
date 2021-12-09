5968f310d8b0a0173ca9be42b468ad7ef2d0e00c
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-09 06:44:42 +0100

    Solve Day 9

--- Day9/Sources/Day9/Day9.swift
+++ Day9/Sources/Day9/Day9.swift
@@ -1,6 +1,75 @@
-public struct Day9 {
-    public private(set) var text = "Hello, World!"
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
 
-    public init() {
+struct DepthMap {
+    private let depthMap: [Point: Int]
+
+    init(_ string: String) {
+        let arrayMap = string.split(separator: "\n")
+            .map { $0.compactMap { Int(String($0)) } }
+        depthMap = Dictionary(uniqueKeysWithValues: arrayMap.enumerated().flatMap { (y, line) in
+            line.enumerated().map { (x, value) in
+                (Point(x: x, y: y), value)
+            }
+        })
+    }
+
+    private var minima: [Point: Int] {
+        depthMap.filter { (key, value) in
+            depthMap.neighbors(of: key).allSatisfy { $0 > value }
+        }
+    }
+
+    var basins: Set<Set<Point>> {
+        Set(minima.keys.map { depthMap.basin(around: $0) })
+    }
+
+    /// Part 1 answer
+    var riskLevel: Int {
+        minima.values
+            .map { $0 + 1 }
+            .reduce(0, +)
+    }
+
+    /// Part 2 answer
+    var basinValue: Int {
+        basins.map(\.count)
+            .sorted()
+            .suffix(3)
+            .reduce(1, *)
+    }
+}
+
+private extension Dictionary where Key == Point, Value == Int {
+    private subscript(x: Int, y: Int) -> Value? {
+        self[Point(x: x, y: y)]
+    }
+
+    private func adjacentCoordinates(of point: Point) -> Set<Point> {
+        Set([
+            Point(x: point.x + 1, y: point.y),
+            Point(x: point.x - 1, y: point.y),
+            Point(x: point.x, y: point.y + 1),
+            Point(x: point.x, y: point.y - 1)
+        ].filter(keys.contains))
+    }
+
+    /// The (up to) 4 neighbors to the left, right, top, and bottom
+    func neighbors(of point: Point) -> [Value] {
+        adjacentCoordinates(of: point).compactMap { self[$0] }
+    }
+
+    func basin(around point: Point, alreadyKnown: Set<Point> = []) -> Set<Point> {
+        let neighbors = adjacentCoordinates(of: point)
+            .filter { !alreadyKnown.contains($0) }
+            .filter { self[$0] != 9 }
+
+        var alreadyKnown = alreadyKnown.union([point])
+        for neighbor in neighbors {
+            alreadyKnown.formUnion(basin(around: neighbor, alreadyKnown: alreadyKnown))
+        }
+        return alreadyKnown
     }
 }
--- Day9/Tests/Day9Tests/Day9Tests.swift
+++ Day9/Tests/Day9Tests/Day9Tests.swift
@@ -2,10 +2,43 @@ import XCTest
 @testable import Day9
 
 final class Day9Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day9().text, "Hello, World!")
+    func testDay9Part1Example() throws {
+        let input = """
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
+        """
+
+        let map = DepthMap(input)
+
+        XCTAssertEqual(map.riskLevel, 15)
+    }
+
+    func testDay9Part1() throws {
+        let map = DepthMap(input)
+
+        print("Day 9 Part 1:", map.riskLevel)
+    }
+
+    func testDay9Part2Example() throws {
+        let input = """
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
+        """
+
+        let map = DepthMap(input)
+
+        XCTAssertEqual(map.basinValue, 1134)
+    }
+
+    func testDay9Part2() throws {
+        let map = DepthMap(input)
+
+        print("Day 9 Part 2:", map.basinValue)
     }
 }
