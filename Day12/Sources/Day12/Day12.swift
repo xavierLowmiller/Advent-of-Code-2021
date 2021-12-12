cb8d38cdaf71e06f031a296f7b2ac9a6cedbacff
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-12 10:09:47 +0100

    Solve Day 12

--- Day12/Sources/Day12/Day12.swift
+++ Day12/Sources/Day12/Day12.swift
@@ -1,6 +1,55 @@
-public struct Day12 {
-    public private(set) var text = "Hello, World!"
+struct Cave: Hashable {
+    let name: Substring
 
-    public init() {
+    var isSmall: Bool {
+        name.allSatisfy(\.isLowercase)
+    }
+
+    static let start = Cave(name: "start")
+    static let end = Cave(name: "end")
+}
+
+struct CaveSystem {
+    let paths: [Cave: Set<Cave>]
+
+    init(_ input: String) {
+        let pairs = input.split(separator: "\n").flatMap {[
+            (Cave(name: $0.split(separator: "-")[0]), Cave(name: $0.split(separator: "-")[1])),
+            (Cave(name: $0.split(separator: "-")[1]), Cave(name: $0.split(separator: "-")[0]))
+        ]}
+
+        paths = Dictionary(grouping: pairs, by: \.0)
+            .mapValues { Set($0.map(\.1)).subtracting([.start]) }
+    }
+
+    func validPaths(
+        currentNode: Cave = .start,
+        pathSoFar: [Cave] = [],
+        canVisitOneCaveTwice: Bool
+    ) -> [[Cave]] {
+
+        let pathSoFar = pathSoFar + [currentNode]
+
+        guard currentNode != .end else { return [pathSoFar] }
+        guard pathSoFar.isValid(for: canVisitOneCaveTwice) else { return [] }
+
+        return paths[currentNode, default: []].flatMap {
+            validPaths(
+                currentNode: $0,
+                pathSoFar: pathSoFar,
+                canVisitOneCaveTwice: canVisitOneCaveTwice
+            )
+        }
+    }
+}
+
+private extension Array where Element == Cave {
+    func isValid(for canVisitOneCaveTwice: Bool) -> Bool {
+        let smallCavesVisited = filter(\.isSmall)
+        if canVisitOneCaveTwice {
+            return smallCavesVisited.count - Set(smallCavesVisited).count <= 1
+        } else {
+            return smallCavesVisited.count == Set(smallCavesVisited).count
+        }
     }
 }
--- Day12/Tests/Day12Tests/Day12Tests.swift
+++ Day12/Tests/Day12Tests/Day12Tests.swift
@@ -2,10 +2,47 @@ import XCTest
 @testable import Day12
 
 final class Day12Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day12().text, "Hello, World!")
+    func testDay12Part1Example() throws {
+        let input = """
+        start-A
+        start-b
+        A-c
+        A-b
+        b-d
+        A-end
+        b-end
+        """
+
+        let caveSystem = CaveSystem(input)
+
+        XCTAssertEqual(caveSystem.validPaths(canVisitOneCaveTwice: false).count, 10)
+    }
+
+    func testDay12Part1() throws {
+        let caveSystem = CaveSystem(input)
+
+        print("Day 12, Part 1:", caveSystem.validPaths(canVisitOneCaveTwice: false).count)
+    }
+
+    func testDay12Part2Example() throws {
+        let input = """
+        start-A
+        start-b
+        A-c
+        A-b
+        b-d
+        A-end
+        b-end
+        """
+
+        let caveSystem = CaveSystem(input)
+
+        XCTAssertEqual(caveSystem.validPaths(canVisitOneCaveTwice: true).count, 36)
+    }
+
+    func testDay12Part2() throws {
+        let caveSystem = CaveSystem(input)
+
+        print("Day 12, Part 2:", caveSystem.validPaths(canVisitOneCaveTwice: true).count)
     }
 }
