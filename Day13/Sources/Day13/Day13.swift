6ae57cbdae0fc3704c81c50f2ae7600fab5bbcd3
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-13 06:37:03 +0100

    Solve Day 13

--- Day13/Sources/Day13/Day13.swift
+++ Day13/Sources/Day13/Day13.swift
@@ -1,6 +1,70 @@
-public struct Day13 {
-    public private(set) var text = "Hello, World!"
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
 
-    public init() {
+extension Point {
+    init<S: StringProtocol>(_ input: S) {
+        x = Int(input.split(separator: ",")[0])!
+        y = Int(input.split(separator: ",")[1])!
+    }
+}
+
+enum FoldInstruction {
+
+    case x(Int)
+    case y(Int)
+
+    init<S: StringProtocol>(_ input: S) {
+        if input.hasPrefix("fold along y=") {
+            self = .y(Int(input.split(separator: "=")[1])!)
+        } else {
+            self = .x(Int(input.split(separator: "=")[1])!)
+        }
+    }
+}
+
+struct Paper {
+    var points: Set<Point>
+
+    init(dots: String) {
+        points = Set(dots.split(separator: "\n").compactMap(Point.init))
+    }
+
+    mutating func performFold<S: StringProtocol>(_ input: S) {
+        switch FoldInstruction(input) {
+        case .x(let foldingLine):
+            points = Set(points.map { point in
+                guard point.x > foldingLine else { return point }
+                let distance = point.x - foldingLine
+
+                return Point(x: point.x - distance * 2, y: point.y)
+            })
+        case .y(let foldingLine):
+            points = Set(points.map { point in
+                guard point.y > foldingLine else { return point }
+                let distance = point.y - foldingLine
+
+                return Point(x: point.x, y: point.y - distance * 2)
+            })
+        }
+    }
+}
+
+extension Paper: CustomStringConvertible {
+    var description: String {
+        let maxX = points.map(\.x).max() ?? 0
+        let maxY = points.map(\.y).max() ?? 0
+
+        var string = ""
+        for y in 0...maxY {
+            var line = ""
+            for x in 0...maxX {
+                line += points.contains(Point(x: x, y: y)) ? "#" : "."
+            }
+            line += "\n"
+            string += line
+        }
+        return String(string.dropLast())
     }
 }
--- Day13/Tests/Day13Tests/Day13Tests.swift
+++ Day13/Tests/Day13Tests/Day13Tests.swift
@@ -2,10 +2,95 @@ import XCTest
 @testable import Day13
 
 final class Day13Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day13().text, "Hello, World!")
+    func testDay13Part1Example() throws {
+        let input = """
+        6,10
+        0,14
+        9,10
+        0,3
+        10,4
+        4,11
+        6,0
+        6,12
+        4,1
+        0,13
+        10,12
+        3,4
+        3,0
+        8,4
+        1,10
+        2,14
+        8,10
+        9,0
+
+        fold along y=7
+        fold along x=5
+        """.components(separatedBy: "\n\n")
+
+        var paper = Paper(dots: input[0])
+
+        paper.performFold(input[1].split(separator: "\n")[0])
+
+        XCTAssertEqual(paper.points.count, 17)
+    }
+
+    func testDay13Part1() throws {
+        let dots = input.components(separatedBy: "\n\n")[0]
+        let folds = input.components(separatedBy: "\n\n")[1].split(separator: "\n")
+
+        var paper = Paper(dots: dots)
+
+        paper.performFold(folds[0])
+        print("Day 13, Part 1:", paper.points.count)
+    }
+
+    func testDay13Part2Example() throws {
+        let input = """
+        6,10
+        0,14
+        9,10
+        0,3
+        10,4
+        4,11
+        6,0
+        6,12
+        4,1
+        0,13
+        10,12
+        3,4
+        3,0
+        8,4
+        1,10
+        2,14
+        8,10
+        9,0
+
+        fold along y=7
+        fold along x=5
+        """
+
+        let dots = input.components(separatedBy: "\n\n")[0]
+        let folds = input.components(separatedBy: "\n\n")[1].split(separator: "\n")
+
+        var paper = Paper(dots: dots)
+
+        for fold in folds {
+            paper.performFold(fold)
+        }
+
+        print("Day 13, Part 2 (Example):", paper.description, separator: "\n")
+    }
+
+    func testDay13Part2() throws {
+        let dots = input.components(separatedBy: "\n\n")[0]
+        let folds = input.components(separatedBy: "\n\n")[1].split(separator: "\n")
+
+        var paper = Paper(dots: dots)
+
+        for fold in folds {
+            paper.performFold(fold)
+        }
+
+        print("Day 13, Part 2:", paper.description, separator: "\n")
     }
 }
