acf47c4dd806e3237a5c09b1f0be3109d79250c6
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-14 06:45:23 +0100

    Solve Day 14

--- Day14/Sources/Day14/Day14.swift
+++ Day14/Sources/Day14/Day14.swift
@@ -1,6 +1,51 @@
-public struct Day14 {
-    public private(set) var text = "Hello, World!"
+struct TwoCharacters: Hashable {
+    let c1: Character
+    let c2: Character
+}
 
-    public init() {
+struct Polymerization {
+    var state: [TwoCharacters: Int]
+    private let replacementRules: [TwoCharacters: Character]
+    private let firstCharacter: Character
+
+    init(initialState: String, replacementRules: String) {
+        self.state = Dictionary(initialState)
+        self.replacementRules = Dictionary(replacementRules)
+        self.firstCharacter = initialState.first!
+    }
+
+    mutating func grow() {
+        var newState: [TwoCharacters: Int] = [:]
+        for (pair, count) in state {
+            let insertedCharacter = replacementRules[pair]!
+            newState[TwoCharacters(c1: pair.c1, c2: insertedCharacter), default: 0] += count
+            newState[TwoCharacters(c1: insertedCharacter, c2: pair.c2), default: 0] += count
+        }
+        state = newState
+    }
+
+    /// Difference between the most common and least common element
+    var elementDiff: Int {
+        var counts = state.reduce(into: [:]) { $0[$1.key.c2, default: 0] += $1.value }
+        counts[firstCharacter, default: 0] += 1
+        return counts.values.max()! - counts.values.min()!
+    }
+}
+
+private extension Dictionary where Key == TwoCharacters, Value == Int {
+    init(_ input: String) {
+        self = zip(input, input.dropFirst()).map(TwoCharacters.init)
+            .reduce(into: [:]) { $0[$1, default: 0] += 1 }
+    }
+}
+
+private extension Dictionary where Key == TwoCharacters, Value == Character {
+    init(_ input: String) {
+        self.init(uniqueKeysWithValues: input.split(separator: "\n").map { line in
+            let c1 = line.first!
+            let c2 = line.dropFirst().first!
+
+            return (TwoCharacters(c1: c1, c2: c2), line.last!)
+        })
     }
 }
--- Day14/Tests/Day14Tests/Day14Tests.swift
+++ Day14/Tests/Day14Tests/Day14Tests.swift
@@ -2,10 +2,96 @@ import XCTest
 @testable import Day14
 
 final class Day14Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day14().text, "Hello, World!")
+    func testDay14Part1Example() throws {
+        let input = """
+        NNCB
+
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
+        """.components(separatedBy: "\n\n")
+
+        let initialState = input[0]
+        let replacementRules = input[1]
+
+        var polymerization = Polymerization(initialState: initialState, replacementRules: replacementRules)
+
+        for _ in 0..<10 {
+            polymerization.grow()
+        }
+        XCTAssertEqual(polymerization.elementDiff, 1588)
+    }
+
+    func testDay14Part1() throws {
+        let initialState = input[0]
+        let replacementRules = input[1]
+
+        var polymerization = Polymerization(initialState: initialState, replacementRules: replacementRules)
+        
+        for _ in 0..<10 {
+            polymerization.grow()
+        }
+
+        print("Day 14, Part 1:", polymerization.elementDiff)
+    }
+
+    func testDay14Part2Example() throws {
+        let input = """
+        NNCB
+
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
+        """.components(separatedBy: "\n\n")
+
+        let initialState = input[0]
+        let replacementRules = input[1]
+
+        var polymerization = Polymerization(initialState: initialState, replacementRules: replacementRules)
+
+        for _ in 0..<40 {
+            polymerization.grow()
+        }
+
+        XCTAssertEqual(polymerization.elementDiff, 2188189693529)
+    }
+
+    func testDay14Part2() throws {
+        let initialState = input[0]
+        let replacementRules = input[1]
+
+        var polymerization = Polymerization(initialState: initialState, replacementRules: replacementRules)
+
+        for _ in 0..<40 {
+            polymerization.grow()
+        }
+
+        print("Day 14, Part 2:", polymerization.elementDiff)
     }
 }
