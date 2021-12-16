98b26b48082578a9e84b82e45f62ab17a7555625
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-16 07:27:30 +0100

    Solve Day 16

--- Day16/Sources/Day16/Day16.swift
+++ Day16/Sources/Day16/Day16.swift
@@ -1,6 +1,130 @@
-public struct Day16 {
-    public private(set) var text = "Hello, World!"
+struct Packet: Equatable {
+    enum Kind: Equatable {
+        enum Operation {
+            case sum
+            case product
+            case minimum
+            case maximum
+            case greaterThan
+            case lessThan
+            case equalTo
 
-    public init() {
+            init<S: StringProtocol>(_ input: S) {
+                switch input {
+                case "000": self = .sum
+                case "001": self = .product
+                case "010": self = .minimum
+                case "011": self = .maximum
+                case "101": self = .greaterThan
+                case "110": self = .lessThan
+                case "111": self = .equalTo
+                default: fatalError()
+                }
+            }
+        }
+
+        case literal(_ value: Int)
+        case `operator`(_ operation: Operation, _ subPackets: [Packet])
+    }
+
+    let version: Int
+    let kind: Kind
+
+    var versionSum: Int {
+        switch kind {
+        case .literal:
+            return version
+        case .operator(_, let subPackets):
+            return version + subPackets.reduce(0) { $0 + $1.versionSum }
+        }
+    }
+
+    var result: Int {
+        switch kind {
+        case .literal(let value):
+            return value
+        case .operator(let operation, let subPackets):
+            let subPackets = subPackets.map(\.result)
+            switch operation {
+            case .sum:
+                return subPackets.reduce(0, +)
+            case .product:
+                return subPackets.reduce(1, *)
+            case .minimum:
+                return subPackets.min()!
+            case .maximum:
+                return subPackets.max()!
+            case .greaterThan:
+                return subPackets[0] > subPackets[1] ? 1 : 0
+            case .lessThan:
+                return subPackets[0] < subPackets[1] ? 1 : 0
+            case .equalTo:
+                return subPackets[0] == subPackets[1] ? 1 : 0
+            }
+        }
+    }
+}
+
+extension Packet {
+    init(_ input: inout Substring) {
+
+        let version = input.prefix(3)
+        input.removeFirst(3)
+        self.version = Int(version, radix: 2)!
+
+        let kind = input.prefix(3)
+        input.removeFirst(3)
+
+        if kind == "100" {
+            // Literal
+            self.kind = .literal(Int(bitString: &input))
+        } else {
+            // Operator
+            let lengthTypeId = input.prefix(1)
+            input.removeFirst(1)
+
+            let operation = Kind.Operation(kind)
+
+            if lengthTypeId == "0" {
+                let lengthString = input.prefix(15)
+                input.removeFirst(15)
+                let length = Int(lengthString, radix: 2)!
+
+                var subPacketsString = input.prefix(length)
+                input.removeFirst(length)
+
+                var packets: [Packet] = []
+                while !subPacketsString.isEmpty {
+                    packets.append(Packet(&subPacketsString))
+                }
+
+                self.kind = .operator(operation, packets)
+            } else {
+                let amountString = input.prefix(11)
+                input.removeFirst(11)
+                let amount = Int(amountString, radix: 2)!
+
+                var packets: [Packet] = []
+                for _ in 0..<amount {
+                    packets.append(Packet(&input))
+                }
+
+                self.kind = .operator(operation, packets)
+            }
+        }
+    }
+}
+
+private extension Int {
+    init(bitString: inout Substring) {
+        var group: Substring = ""
+        var binary: Substring = ""
+        repeat {
+            group = bitString.prefix(5)
+            bitString.removeFirst(5)
+            binary.append(contentsOf: group.suffix(4))
+        } while group.first == "1"
+
+        self = Int(binary, radix: 2)!
     }
 }
--- Day16/Tests/Day16Tests/Day16Tests.swift
+++ Day16/Tests/Day16Tests/Day16Tests.swift
@@ -2,10 +2,181 @@ import XCTest
 @testable import Day16
 
 final class Day16Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day16().text, "Hello, World!")
+    func testDay16Part1Example1() throws {
+        var input = """
+        D2FE28
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+
+        let expected = Packet(version: 6, kind: .literal(2021))
+
+        XCTAssertEqual(packet, expected)
+        XCTAssertEqual(packet.versionSum, 6)
+    }
+
+    func testDay16Part1Example2() throws {
+        var input = """
+        38006F45291200
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+
+        let expected = Packet(version: 1, kind: .operator(.lessThan, [
+            Packet(version: 6, kind: .literal(10)),
+            Packet(version: 2, kind: .literal(20))
+        ]))
+
+        XCTAssertEqual(packet, expected)
+        XCTAssertEqual(packet.versionSum, 9)
+    }
+
+    func testDay16Part1Example3() throws {
+        var input = """
+        EE00D40C823060
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+
+        let expected = Packet(version: 7, kind: .operator(.maximum, [
+            Packet(version: 2, kind: .literal(1)),
+            Packet(version: 4, kind: .literal(2)),
+            Packet(version: 1, kind: .literal(3))
+        ]))
+
+        XCTAssertEqual(packet, expected)
+        XCTAssertEqual(packet.versionSum, 14)
+    }
+
+    func testDay16Part1Example4() throws {
+        var input = """
+        8A004A801A8002F478
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+
+        let expected = Packet(version: 4, kind: .operator(.minimum, [
+            Packet(version: 1, kind: .operator(.minimum, [
+                Packet(version: 5, kind: .operator(.minimum, [
+                    Packet(version: 6, kind: .literal(15))
+                ]))
+            ]))
+        ]))
+
+        XCTAssertEqual(packet, expected)
+        XCTAssertEqual(packet.versionSum, 16)
+    }
+
+    func testDay16Part1Example5() throws {
+        var input = """
+        620080001611562C8802118E34
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.versionSum, 12)
+    }
+
+    func testDay16Part1Example6() throws {
+        var input = """
+        C0015000016115A2E0802F182340
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.versionSum, 23)
+    }
+
+    func testDay16Part1Example7() throws {
+        var input = """
+        A0016C880162017C3686B18A3D4780
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.versionSum, 31)
+    }
+
+    func testDay16Part1() throws {
+        var input = input.expandedHexString[...]
+
+        let packet = Packet(&input)
+        print("Day 16 Part 1:", packet.versionSum)
+    }
+
+    func testDay16Part2Example1() {
+        var input = """
+        C200B40A82
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 3)
+    }
+
+    func testDay16Part2Example2() {
+        var input = """
+        04005AC33890
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 54)
+    }
+
+    func testDay16Part2Example3() {
+        var input = """
+        880086C3E88112
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 7)
+    }
+
+    func testDay16Part2Example4() {
+        var input = """
+        CE00C43D881120
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 9)
+    }
+
+    func testDay16Part2Example5() {
+        var input = """
+        D8005AC2A8F0
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 1)
+    }
+
+    func testDay16Part2Example6() {
+        var input = """
+        F600BC2D8F
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 0)
+    }
+
+    func testDay16Part2Example7() {
+        var input = """
+        9C005AC2F8F0
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 0)
+    }
+
+    func testDay16Part2Example8() {
+        var input = """
+        9C0141080250320F1802104A08
+        """.expandedHexString[...]
+
+        let packet = Packet(&input)
+        XCTAssertEqual(packet.result, 1)
+    }
+
+    func testDay16Part2() throws {
+        var input = input.expandedHexString[...]
+
+        let packet = Packet(&input)
+        print("Day 16 Part 2:", packet.result)
     }
 }
