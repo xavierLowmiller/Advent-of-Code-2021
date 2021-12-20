c578d7cefdb45e101a23972d2ecdedb922b4d38c
Xavier Lowmiller <xaver.lohmueller@me.com>
2021-12-20 21:32:00 +0100

    Solve Day 20

--- Day20/Sources/Day20/Day20.swift
+++ Day20/Sources/Day20/Day20.swift
@@ -1,6 +1,92 @@
-public struct Day20 {
-    public private(set) var text = "Hello, World!"
+struct Point: Hashable {
+    let x: Int
+    let y: Int
 
-    public init() {
+    var neighbors: [Point] {
+        [
+            Point(x: x - 1, y: y - 1),
+            Point(x: x    , y: y - 1),
+            Point(x: x + 1, y: y - 1),
+            Point(x: x - 1, y: y    ),
+            Point(x: x    , y: y    ),
+            Point(x: x + 1, y: y    ),
+            Point(x: x - 1, y: y + 1),
+            Point(x: x    , y: y + 1),
+            Point(x: x + 1, y: y + 1)
+        ]
+    }
+}
+
+struct Image: CustomStringConvertible {
+    var data: [Point: Bool]
+
+    init<S: StringProtocol>(_ input: S) {
+        data = Dictionary(uniqueKeysWithValues: input.split(separator: "\n").enumerated().flatMap { (y, line) in
+            line.enumerated().map { (x, char) in
+                (Point(x: x, y: y), char == "#")
+            }
+        })
+    }
+
+    var description: String {
+        let minX = data.keys.map(\.x).min()!
+        let maxX = data.keys.map(\.x).max()!
+        let minY = data.keys.map(\.y).min()!
+        let maxY = data.keys.map(\.y).max()!
+
+        var result = ""
+        for y in minY...maxY {
+            for x in minX...maxX {
+                result.append(data[Point(x: x, y: y), default: false] ? "#" : ".")
+            }
+            result.append("\n")
+        }
+        return result
+    }
+
+    /// Adds a 1-pixel-wide border to the image
+    /// (so the enhancement can always work)
+    private mutating func expand(_ value: Bool = false) {
+        let minX = data.keys.map(\.x).min()! - 1
+        let maxX = data.keys.map(\.x).max()! + 1
+        let minY = data.keys.map(\.y).min()! - 1
+        let maxY = data.keys.map(\.y).max()! + 1
+
+        for y in minY...maxY {
+            for x in minX...maxX {
+                let point = Point(x: x, y: y)
+                if !data.keys.contains(point) {
+                    data[point] = value
+                }
+            }
+        }
+    }
+
+    mutating func enhanceTwice(using algorithm: [Bool]) {
+        expand(false)
+
+        var newData: [Point: Bool] = [:]
+
+        for point in data.keys {
+            let neighbors = point.neighbors
+            let lookupValue = numberValue(for: neighbors, default: false)
+            newData[point] = algorithm[lookupValue]
+        }
+
+        data = newData
+
+        expand(algorithm[0])
+
+        for point in data.keys {
+            let neighbors = point.neighbors
+            let lookupValue = numberValue(for: neighbors, default: algorithm[0])
+            newData[point] = algorithm[lookupValue]
+        }
+
+        data = newData
+    }
+
+    private func numberValue(for points: [Point], default: Bool) -> Int {
+        Int(String(points.map { data[$0, default: `default`] ? "1" : "0" }), radix: 2)!
     }
 }
--- Day20/Tests/Day20Tests/Day20Tests.swift
+++ Day20/Tests/Day20Tests/Day20Tests.swift
@@ -2,10 +2,77 @@ import XCTest
 @testable import Day20
 
 final class Day20Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day20().text, "Hello, World!")
+    func testDay20Part1Example() throws {
+        let input = """
+        ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+        #..#.
+        #....
+        ##..#
+        ..#..
+        ..###
+        """.components(separatedBy: "\n\n")
+
+        let algorithm = input[0].map { $0 == "#" ? true : false }
+        XCTAssertEqual(algorithm.count, 512)
+
+        var image = Image(input[1])
+        XCTAssertEqual(image.data.filter(\.value).count, 10)
+
+        image.enhanceTwice(using: algorithm)
+
+        XCTAssertEqual(image.data.filter(\.value).count, 35)
+    }
+
+    func testDay20Part1() throws {
+        let input = input.components(separatedBy: "\n\n")
+
+        let algorithm = input[0].map { $0 == "#" ? true : false }
+        XCTAssertEqual(algorithm.count, 512)
+
+        var image = Image(input[1])
+
+        image.enhanceTwice(using: algorithm)
+
+        print("Day 20, Part 1:", image.data.filter(\.value).count)
+    }
+
+    func testDay20Part2Example() throws {
+        let input = """
+        ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+        #..#.
+        #....
+        ##..#
+        ..#..
+        ..###
+        """.components(separatedBy: "\n\n")
+
+        let algorithm = input[0].map { $0 == "#" ? true : false }
+        XCTAssertEqual(algorithm.count, 512)
+
+        var image = Image(input[1])
+        XCTAssertEqual(image.data.filter(\.value).count, 10)
+
+        for _ in 0..<25 {
+            image.enhanceTwice(using: algorithm)
+        }
+
+        XCTAssertEqual(image.data.filter(\.value).count, 3351)
+    }
+
+    func testDay20Part2() throws {
+        let input = input.components(separatedBy: "\n\n")
+
+        let algorithm = input[0].map { $0 == "#" ? true : false }
+        XCTAssertEqual(algorithm.count, 512)
+
+        var image = Image(input[1])
+
+        for _ in 0..<25 {
+            image.enhanceTwice(using: algorithm)
+        }
+
+        print("Day 20, Part 2:", image.data.filter(\.value).count)
     }
 }
