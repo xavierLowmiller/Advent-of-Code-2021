5f5d715b594b504c85b1ea8997a8fb51e1590738
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-10-30 20:13:07 +0100

    Solve Day 25

--- Day25/Sources/Day25/Day25.swift
+++ Day25/Sources/Day25/Day25.swift
@@ -1,6 +1,110 @@
-public struct Day25 {
-    public private(set) var text = "Hello, World!"
+struct Point: Hashable {
+    var x: Int
+    var y: Int
+}
 
-    public init() {
+struct Seafloor: CustomStringConvertible, Equatable {
+    var eastBound: Set<Point> = []
+    var southBound: Set<Point> = []
+
+    private var maxX: Int
+    private var maxY: Int
+
+    public init(_ string: String) {
+        let lines = string.split(separator: "\n")
+        var x = 0
+        var y = 0
+        maxX = 0
+        for line in lines {
+            for char in line {
+                switch char {
+                case ">":
+                    eastBound.insert(Point(x: x, y: y))
+                case "v":
+                    southBound.insert(Point(x: x, y: y))
+                default:
+                    break
+                }
+                x += 1
+            }
+            maxX = x - 1
+            x = 0
+            y += 1
+        }
+        maxY = y - 1
+        sanityCheck()
+    }
+
+    mutating func step() {
+        var newEastBound: Set<Point> = []
+        var newSouthBound: Set<Point> = []
+
+        for point in eastBound {
+            let newPoint = Point(x: (point.x + 1) % (maxX + 1), y: point.y)
+            if eastBound.contains(newPoint) || southBound.contains(newPoint) {
+                newEastBound.insert(point)
+            } else {
+                newEastBound.insert(newPoint)
+            }
+        }
+        eastBound = newEastBound
+        sanityCheck()
+
+        for point in southBound {
+            let newPoint = Point(x: point.x, y: (point.y + 1) % (maxY + 1))
+            if eastBound.contains(newPoint) || southBound.contains(newPoint) {
+                newSouthBound.insert(point)
+            } else {
+                newSouthBound.insert(newPoint)
+            }
+        }
+
+        southBound = newSouthBound
+        sanityCheck()
+    }
+
+    /// Call `step` until there is no more movement
+    /// - Returns: the number of steps it took to cease all movement
+    mutating func stepUntilStuck() -> Int {
+
+        var oldEastBound: Set<Point> = []
+        var oldSouthBound: Set<Point> = []
+        var steps = 0
+        repeat {
+            oldEastBound = eastBound
+            oldSouthBound = southBound
+            step()
+            steps += 1
+        } while oldEastBound != eastBound || oldSouthBound != southBound
+
+        return steps
+    }
+
+    private func sanityCheck() {
+        assert(eastBound.map(\.x).max() ?? 0 <= maxX)
+        assert(eastBound.map(\.y).max() ?? 0 <= maxY)
+        assert(southBound.map(\.x).max() ?? 0 <= maxX)
+        assert(southBound.map(\.y).max() ?? 0 <= maxY)
+        assert(eastBound.intersection(southBound).isEmpty)
+    }
+
+    var description: String {
+        var description = ""
+        for y in 0...maxY {
+            for x in 0...maxX {
+                if eastBound.contains(Point(x: x, y: y)) {
+                    description += ">"
+                } else if southBound.contains(Point(x: x, y: y)) {
+                    description += "v"
+                } else {
+                    description += "."
+                }
+            }
+            description += "\n"
+        }
+
+        description.removeLast()
+
+        return description
     }
 }
--- Day25/Tests/Day25Tests/Day25Tests.swift
+++ Day25/Tests/Day25Tests/Day25Tests.swift
@@ -2,10 +2,173 @@ import XCTest
 @testable import Day25
 
 final class Day25Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Day25().text, "Hello, World!")
+    func testParsingExample1() {
+        // Given
+        let input = "...>>>>>..."
+
+        // When
+        let seafloor = Seafloor(input)
+
+        // Then
+        XCTAssertEqual(seafloor.description, input)
+    }
+
+    func testParsingExample2() {
+        // Given
+        let input = """
+        ..........
+        .>v....v..
+        .......>..
+        ..........
+        """
+
+        // When
+        let seafloor = Seafloor(input)
+
+        // Then
+        XCTAssertEqual(seafloor.description, input)
+    }
+
+    func testStepExample1() {
+        // Given
+        let input = "...>>>>>..."
+        var seafloor = Seafloor(input)
+
+        // When
+        seafloor.step()
+
+        // Then
+        XCTAssertEqual(seafloor.description, "...>>>>.>..")
+
+        // When
+        seafloor.step()
+
+        // Then
+        XCTAssertEqual(seafloor.description, "...>>>.>.>.")
+    }
+
+    func testStepExample2() {
+        // Given
+        let input = """
+        ..........
+        .>v....v..
+        .......>..
+        ..........
+        """
+        var seafloor = Seafloor(input)
+
+        // When
+        seafloor.step()
+
+        // Then
+        XCTAssertEqual(seafloor.description, """
+        ..........
+        .>........
+        ..v....v>.
+        ..........
+        """)
+    }
+
+    func testStepExample3() {
+        // Given
+        let input = """
+        ...>...
+        .......
+        ......>
+        v.....>
+        ......>
+        .......
+        ..vvv..
+        """
+        var seafloor = Seafloor(input)
+
+        // When 1
+        seafloor.step()
+
+        // Then 1
+        XCTAssertEqual(seafloor.description, """
+        ..vv>..
+        .......
+        >......
+        v.....>
+        >......
+        .......
+        ....v..
+        """)
+
+        // When 2
+        seafloor.step()
+
+        // Then 2
+        XCTAssertEqual(seafloor.description, """
+        ....v>.
+        ..vv...
+        .>.....
+        ......>
+        v>.....
+        .......
+        .......
+        """)
+
+        // When 3
+        seafloor.step()
+
+        // Then 3
+        XCTAssertEqual(seafloor.description, """
+        ......>
+        ..v.v..
+        ..>v...
+        >......
+        ..>....
+        v......
+        .......
+        """)
+
+        // When 4
+        seafloor.step()
+
+        // Then 4
+        XCTAssertEqual(seafloor.description, """
+        >......
+        ..v....
+        ..>.v..
+        .>.v...
+        ...>...
+        .......
+        v......
+        """)
+    }
+
+    func testSteppingUntilStuck() {
+        // Given
+        let input = """
+        v...>>.vv>
+        .vv>>.vv..
+        >>.>v>...v
+        >>v>>.>.v.
+        v>v.vv.v..
+        >.>>..v...
+        .vv..>.>v.
+        v.v..>>v.v
+        ....v..v.>
+        """
+        var seafloor = Seafloor(input)
+
+        // When
+        let steps = seafloor.stepUntilStuck()
+
+        // Then
+        XCTAssertEqual(steps, 58)
+    }
+
+    func testDay25() {
+        // Given
+        var seafloor = Seafloor(input)
+
+        // When
+        let steps = seafloor.stepUntilStuck()
+
+        // Then
+        print("Day 25:", steps)
     }
 }
